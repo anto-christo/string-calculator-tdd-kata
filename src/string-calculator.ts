fd8b432e768a4440ec92216069ea5ccafc3dd1af
anto-christo <anto.christo.20@gmail.com>
2023-12-16 15:58:29 +0530

    feat: add functionality to handle two numbers

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -1,4 +1,9 @@
 export function add(numbers: string): number {
+  if (numbers.length === 3) {
+    const [firstNumber, secondNumber] = numbers.split(',');
+    return Number(firstNumber) + Number(secondNumber);
+  }
+
   if (numbers.length === 1) {
     return Number(numbers);
   }
