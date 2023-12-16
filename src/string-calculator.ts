aeb3e562301654a992b7de11ff0aaf96adea84c5
anto-christo <anto.christo.20@gmail.com>
2023-12-16 16:12:42 +0530

    feat: add functionaly to handle newline separator along with comma separator

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -4,6 +4,6 @@
  * @returns the sum of given numbers
  */
 export function add(numbers: string): number {
-  const numbersToAdd = numbers.split(',').map(number => Number(number));
+  const numbersToAdd = numbers.split(/,|\n/).map(number => Number(number));
   return numbersToAdd.reduce((sum, number) => sum + number, 0)
 }
