6fb2acfad5683d22a34cf981f2a43ba0a713edd5
anto-christo <anto.christo.20@gmail.com>
2023-12-16 15:56:04 +0530

    chore: add test case for two numbers as input

--- tests/string-calculator.test.ts
+++ tests/string-calculator.test.ts
@@ -1,13 +1,18 @@
 import { add } from "../src/string-calculator";
 
 describe('The string calculator', () => {
-  test('should return zero for an empty string', () => {
+  test('should return zero for an empty input string', () => {
     const result = add("");
     expect(result).toBe(0);
   })
 
-  test('should return the same number if the string only has a single number', () => {
+  test('should return the same number if the input string only has a single number', () => {
     const result = add("1");
     expect(result).toBe(1);
+  })
+
+  test('should return the sum of two numbers if the input string has two comma-separated numbers', () => {
+    const result = add("1,2");
+    expect(result).toBe(3);
   })
 })
