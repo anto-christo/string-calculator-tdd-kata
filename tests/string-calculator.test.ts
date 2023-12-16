574af7d14844fa0723310451a5251db3adab3f87
anto-christo <anto.christo.20@gmail.com>
2023-12-16 15:50:38 +0530

    chore: add test for a single number as input

--- tests/string-calculator.test.ts
+++ tests/string-calculator.test.ts
@@ -5,4 +5,9 @@ describe('The string calculator', () => {
     const result = add("");
     expect(result).toBe(0);
   })
+
+  test('should return the same number if the string only has a single number', () => {
+    const result = add("1");
+    expect(result).toBe(1);
+  })
 })
