c20cf4b7d9d5985e37a7897056a4902908ecf598
anto-christo <anto.christo.20@gmail.com>
2023-12-16 16:24:40 +0530

    feat: add functionality to handle custom separators

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -4,6 +4,17 @@
  * @returns the sum of given numbers
  */
 export function add(numbers: string): number {
-  const numbersToAdd = numbers.split(/,|\n/).map(number => Number(number));
-  return numbersToAdd.reduce((sum, number) => sum + number, 0)
+  let separator: RegExp | string = /,|\n/;
+
+  if (numbers.startsWith("//")) {
+    const [separatorLine, numbersLine] = numbers.split('\n');
+
+    if (separatorLine && numbersLine) {
+      separator = separatorLine.substring(2);
+      numbers = numbersLine;
+    }
+  }
+
+  const numbersToAdd = numbers.split(separator).map(number => Number(number));
+  return numbersToAdd.reduce((sum, number) => sum + number, 0);
 }
